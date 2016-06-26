26798f5311965b387aadf3ef6d0d6a8aeaf3703e
Travis Gorman <thefarbetween@me.com>
2016-06-26 15:41:06 -0500

    assign variables to all keys

---
+++ main.js
@@ -0,0 +1,19 @@
+// define variables
+// map variables onto all input buttons
+var display = document.querySelector('#display');
+var zero = document.querySelector('#b0');
+var one = document.querySelector('#b1');
+var two = document.querySelector('#b2');
+var three = document.querySelector('#b3');
+var four = document.querySelector('#b4');
+var five = document.querySelector('#b5');
+var six = document.querySelector('#b6');
+var seven = document.querySelector('#b7');
+var eight = document.querySelector('#b8');
+var nine = document.querySelector('#b9');
+var add = document.querySelector('#add');
+var subtract = document.querySelector('#subtract');
+var multiply = document.querySelector('#multiply');
+var divide = document.querySelector('#divide');
+var clear = document.querySelector('#clear');
+var calc = document.querySelector('#calc');
