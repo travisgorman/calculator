ec0bcd4874e4797f6cc409bfd013976ca210b622
Travis Gorman <thefarbetween@me.com>
2016-06-26 15:51:55 -0500

    write listeners for number keys

--- main.js
+++ main.js
@@ -17,3 +17,45 @@ var multiply = document.querySelector('#multiply');
 var divide = document.querySelector('#divide');
 var clear = document.querySelector('#clear');
 var calc = document.querySelector('#calc');
+
+// add event listeners
+// show a pre-defined value on the display value upon click
+one.addEventListener('click', function(){
+  display.value += 1;
+});
+
+two.addEventListener('click', function(){
+  display.value += 2;
+});
+
+three.addEventListener('click', function(){
+  display.value += 3;
+});
+
+four.addEventListener('click', function(){
+  display.value += 4;
+});
+
+five.addEventListener('click', function(){
+  display.value += 5;
+});
+
+six.addEventListener('click', function(){
+  display.value += 6;
+});
+
+seven.addEventListener('click', function(){
+  display.value += 7;
+});
+
+eight.addEventListener('click', function(){
+  display.value += 8;
+});
+
+nine.addEventListener('click', function(){
+  display.value += 9;
+});
+
+zero.addEventListener('click', function(){
+  display.value += 0;
+});
