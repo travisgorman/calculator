51b2dec15926807b06437a01dc5ec7060a2cfd6c
Travis Gorman <thefarbetween@me.com>
2016-06-26 20:20:49 -0500

    does all 4 math operations, but only with single digits

--- main.js
+++ main.js
@@ -1,147 +1,142 @@
-// define variables
-// map variables onto all input buttons
-var output = document.querySelector('#output');
-var zero = document.querySelector('#b0');
-var one = document.querySelector('#b1');
-var two = document.querySelector('#b2');
-var three = document.querySelector('#b3');
-var four = document.querySelector('#b4');
-var five = document.querySelector('#b5');
-var six = document.querySelector('#b6');
-var seven = document.querySelector('#b7');
-var eight = document.querySelector('#b8');
-var nine = document.querySelector('#b9');
-var add = document.querySelector('#add');
+
+var output = document.querySelector('output');
+
+
+
+var number1 = document.querySelector('#b1');
+number1.addEventListener('click',function(){
+
+output.value += 1;
+
+});
+
+var number2 = document.querySelector('#b2');
+number2.addEventListener('click',function(){
+// var output = document.querySelector('#output');
+output.value +=2;
+});
+
+
+var number3 = document.querySelector('#b3');
+number3.addEventListener('click',function(){
+// var output = document.querySelector('#output');
+output.value+=3;
+});
+
+var plus = document.querySelector('#plus');
+plus.addEventListener('click',function(){
+// var output = document.querySelector('#output');
+output.value+= "+";
+});
+
+var number4 = document.querySelector('#b4');
+number4.addEventListener('click',function() {
+// var output = document.querySelector('#output');
+output.value+=4;
+});
+
+
+
+var number5 = document.querySelector('#b5');
+number5.addEventListener('click',function() {
+// var output = document.querySelector('#output');
+output.value+=5;
+});
+
+
+var number6 = document.querySelector('#b6');
+number6.addEventListener('click',function() {
+// var output = document.querySelector('#output');
+output.value+=6;
+});
+
+
 var subtract = document.querySelector('#subtract');
+subtract.addEventListener('click',function() {
+// var output = document.querySelector('#output');
+output.value+= "-";
+});
+
+var number7 = document.querySelector('#b7');
+number7.addEventListener('click',function() {
+// var output = document.querySelector('#output');
+output.value+=7;
+});
+
+
+var number8 = document.querySelector('#b8');
+number8.addEventListener('click',function() {
+// var output = document.querySelector('#output');
+output.value+=8;
+});
+
+var number9 = document.querySelector('#b9');
+number9.addEventListener('click',function() {
+// var output = document.querySelector('#output');
+output.value+=9;
+});
+
+
+var number0= document.querySelector('#b0');
+number0.addEventListener('click',function() {
+// var output = document.querySelector('#output');
+output.value+=0;
+console.log(output.value, typeof(output.value));
+});
+
+
 var multiply = document.querySelector('#multiply');
+multiply.addEventListener('click',function() {
+// var output = document.querySelector('#output');
+output.value+= "*";
+});
+
 var divide = document.querySelector('#divide');
+divide.addEventListener('click',function() {
+// var output = document.querySelector('#output');
+output.value+= "/";
+});
+
 var clear = document.querySelector('#clear');
-var calc = document.querySelector('#calc');
-
-//add event listeners
-// add a pre-defined value onto the output value upon click
-
-one.addEventListener('click', function(){
-  output.value += 1;
-  console.dir( one );
-
+clear.addEventListener('click',function() {
+output.value = '';
+//total = '';
+//output.textContent = '';
 });
 
-two.addEventListener('click', function(){
-  output.value += 2;
-  console.dir( two );
-
-});
-
-three.addEventListener('click', function(){
-  output.value += 3;
-  console.dir( three );
-
-});
-
-four.addEventListener('click', function(){
-  output.value += 4;
-  console.dir( four );
-
-});
-
-five.addEventListener('click', function(){
-  output.value += 5;
-  console.dir( five );
-
-});
-
-six.addEventListener('click', function(){
-  output.value += 6;
-  console.dir( six );
-
-});
-
-seven.addEventListener('click', function(){
-  output.value += 7;
-  console.dir( seven.value );
-
-});
-
-eight.addEventListener('click', function(){
-  output.value += 8;
-  console.dir( eight.attributes );
-
-});
-
-nine.addEventListener('click', function(){
-  output.value += 9;
-  console.dir( nine );
-
-});
-
-zero.addEventListener('click', function(){
-  output.value += 0;
-  console.dir( zero );
-});
-
-add.addEventListener('click', function(){
-  output.value +='+';
-});
-
-subtract.addEventListener('click', function(){
-  output.value +='-';
-});
-
-multiply.addEventListener('click', function(){
-  output.value +='x';
-});
-
-divide.addEventListener('click', function(){
-  output.value +='/';
-});
-
-clear.addEventListener('click', function(){
-  output.value +='';
-});
-
-// on click, the `calc` key does math accordingly
-// and writes the calculation to the output
 
 
-var calculate = document.querySelector('#calc');
 
+
+var calculate = document.querySelector('#calculate');
 calculate.addEventListener('click',function(){
 
-  var operation = output.innerText;
-  var num1 = operation[0];
-  var num2 = operation[2];
-  var operator = operation[1];
-  var total = '';
 
-  if (operator === '+'){
-    total = Number(num1) + Number(num2);
-    output.innerText = total;
-  }
-  else if (operator === '-'){
-    total = Number(num1) - Number(num2);
-    output.value = total;
-    output.innerText = total;
+var operation = output.innerText;
+var num1 = operation[0];
+var num2 = operation[2];
+var operator = operation[1];
+var total = '';
+
+if (operator === '+'){
+ total = Number(num1) + Number(num2);
+
+ output.innerText = total;
+}
+    else if (operator === '-'){
+   total = Number(num1) - Number(num2);
+output.value = total;
+output.innerText = total;
  }
-  else if (operator === '*'){
-    total = Number(num1) * Number(num2);
-    output.value = total;
-    output.innerText = total;
-  }
-  else if (operator === '/'){
-    total = Number(num1) / Number(num2);
-    output.value = total;
-    output.innerText = total;
-  }
-});
-// calc.addEventListener('click', function(){
-//
-//   var
-//
-//
-// });
-// calc.addEventListener('click', function(){
-//   output.value +='='
-// });
 
-//
+else if (operator === '*'){
+total = Number(num1) * Number(num2);
+output.value = total;
+output.innerText = total;
+}
+else if (operator === '/'){
+total = Number(num1) / Number(num2);
+output.value = total;
+output.innerText = total;
+}
+});
