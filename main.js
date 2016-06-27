4c47252246de884118148db9b72d950cc8dd0e8e
Travis Gorman <thefarbetween@me.com>
2016-06-26 22:43:59 -0500

    style buttons and display

--- main.js
+++ main.js
@@ -1,142 +1,69 @@
+var numberButtons = document.querySelectorAll('.numbers button');
+var operatorButtons = document.querySelectorAll('.operators button');
+var answerArea = document.getElementById('answer');
+var clearButton = document.querySelector('.clear');
+var equalsButton = document.querySelector('.equals');
+var firstNumber;
+var operator;
 
-var output = document.querySelector('output');
-
-
-
-var number1 = document.querySelector('#b1');
-number1.addEventListener('click',function(){
-
-output.value += 1;
-
-});
-
-var number2 = document.querySelector('#b2');
-number2.addEventListener('click',function(){
-// var output = document.querySelector('#output');
-output.value +=2;
-});
-
-
-var number3 = document.querySelector('#b3');
-number3.addEventListener('click',function(){
-// var output = document.querySelector('#output');
-output.value+=3;
-});
-
-var plus = document.querySelector('#plus');
-plus.addEventListener('click',function(){
-// var output = document.querySelector('#output');
-output.value+= "+";
-});
-
-var number4 = document.querySelector('#b4');
-number4.addEventListener('click',function() {
-// var output = document.querySelector('#output');
-output.value+=4;
-});
+// var clk = document.querySelectorAll('numbers').addEventListener('click', function(e){
+//   console.log(e.target.innerHTML);
+//   console.log(answerArea.innerHTML);
+// } )
+// numberButtons.addEventListener('click', function(){
+//   console.log(newNumberVal);
+//   console.log(oldNumberVal);
+//   console.dir(answerArea.innerHTML)
+// })
 
 
-
-var number5 = document.querySelector('#b5');
-number5.addEventListener('click',function() {
-// var output = document.querySelector('#output');
-output.value+=5;
-});
-
-
-var number6 = document.querySelector('#b6');
-number6.addEventListener('click',function() {
-// var output = document.querySelector('#output');
-output.value+=6;
-});
+document.querySelector('.numbers').addEventListener('click', function(e){
+  console.log(answerArea.innerHTML);
+  console.log(e.target.innerHTML);
+  console.log(e);
+})
 
 
-var subtract = document.querySelector('#subtract');
-subtract.addEventListener('click',function() {
-// var output = document.querySelector('#output');
-output.value+= "-";
-});
-
-var number7 = document.querySelector('#b7');
-number7.addEventListener('click',function() {
-// var output = document.querySelector('#output');
-output.value+=7;
-});
-
-
-var number8 = document.querySelector('#b8');
-number8.addEventListener('click',function() {
-// var output = document.querySelector('#output');
-output.value+=8;
-});
-
-var number9 = document.querySelector('#b9');
-number9.addEventListener('click',function() {
-// var output = document.querySelector('#output');
-output.value+=9;
-});
-
-
-var number0= document.querySelector('#b0');
-number0.addEventListener('click',function() {
-// var output = document.querySelector('#output');
-output.value+=0;
-console.log(output.value, typeof(output.value));
-});
-
-
-var multiply = document.querySelector('#multiply');
-multiply.addEventListener('click',function() {
-// var output = document.querySelector('#output');
-output.value+= "*";
-});
-
-var divide = document.querySelector('#divide');
-divide.addEventListener('click',function() {
-// var output = document.querySelector('#output');
-output.value+= "/";
-});
-
-var clear = document.querySelector('#clear');
-clear.addEventListener('click',function() {
-output.value = '';
-//total = '';
-//output.textContent = '';
-});
-
-
-
-
-
-var calculate = document.querySelector('#calculate');
-calculate.addEventListener('click',function(){
-
-
-var operation = output.innerText;
-var num1 = operation[0];
-var num2 = operation[2];
-var operator = operation[1];
-var total = '';
-
-if (operator === '+'){
- total = Number(num1) + Number(num2);
-
- output.innerText = total;
+for (var i = 0; i < numberButtons.length; i++) {
+  numberButtons[i].addEventListener('click', function(e) {
+    // debugger;
+    var newNumberVal = e.target.innerHTML;
+    var oldNumberVal = document.getElementById('answer').innerHTML;
+    if (answerArea.innerHTML.length < 7) {
+      document.getElementById('answer').innerHTML = oldNumberVal+newNumberVal;
+    } else {
+      alert('Sorry, that number is too big. We can\'t math that hard');
+    }
+  });
 }
-    else if (operator === '-'){
-   total = Number(num1) - Number(num2);
-output.value = total;
-output.innerText = total;
- }
+//
+for (var i = 0; i < operatorButtons.length; i++) {
+  operatorButtons[i].addEventListener('click', function(e) {
+    // debugger;
+    firstNumber = answerArea.innerHTML;
+    operator = e.target.innerHTML;
+    answerArea.innerHTML = '';
+  });
+}
 
-else if (operator === '*'){
-total = Number(num1) * Number(num2);
-output.value = total;
-output.innerText = total;
-}
-else if (operator === '/'){
-total = Number(num1) / Number(num2);
-output.value = total;
-output.innerText = total;
-}
+clearButton.addEventListener('click', function() {
+  answerArea.innerHTML = '';
+});
+
+equalsButton.addEventListener('click', function() {
+  var answer;
+  if (operator === '+') {
+    answer = Number(firstNumber) + Number(answerArea.innerHTML);
+  } else if (operator === '-') {
+    answer = Number(firstNumber) - Number(answerArea.innerHTML);
+  } else if (operator === 'x') {
+    answer = Number(firstNumber) * Number(answerArea.innerHTML);
+  } else {
+    answer = Number(firstNumber) / Number(answerArea.innerHTML);
+  }
+  if (answer > 9999999) {
+    answerArea.innerHTML = 'too big';
+  } else {
+    answerArea.innerHTML = String(answer).slice(0, 7);
+  }
 });
